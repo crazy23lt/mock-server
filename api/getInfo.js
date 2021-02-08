de285918f84ecf56f6d8a4ac87dcc9cde22a0e9a
刘涛 <liutao@zhuolipu.com>
2021-02-08 17:53:54 +0800

    用户信息接口更新

--- api/getInfo.js
+++ api/getInfo.js
@@ -1,11 +1,12 @@
 const Mock = require('mockjs');
 const Random = Mock.Random;
 module.exports = async (req, res) => {
-    var name = Mock.mock(() => Random.cname())
     res.send({
         meta: "请求成功",
         data: {
-            name
+            name: Mock.mock(() => Random.cname()), phone: 13636065890, auth: Mock.mock({
+                "boolean|1": true
+            }).boolean
         },
         code: 200,
     })
